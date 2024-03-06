d881e994f6f68b653422cacc6f448affc2b568fa
neves103 <ewertonneves103@gmail.com>
2024-03-06 11:16:26 -0300

    primeiro comit

---
+++ App.tsx
@@ -0,0 +1,26 @@
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
+
+import { Loading } from '@components/Loading';
+
+import { Groups } from '@screens/Groups';
+import theme from '@theme/index';
+
+export default function App() {
+const[fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+  return (
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent={true}
+      />
+
+      { fontsLoaded ? <Groups/> : <Loading/> } 
+    </ThemeProvider>
+  );
+}
+
+
